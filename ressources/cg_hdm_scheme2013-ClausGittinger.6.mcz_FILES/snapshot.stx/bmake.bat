507ae21494121e94c9e96b00b120d4426aa54c1a
Jan Mueller <jan.muller87@gmail.com>
2014-01-16 11:10:48 +0100

    removed ressources

--- ressources/cg_hdm_scheme2013-ClausGittinger.6.mcz_FILES/snapshot.stx/bmake.bat
+++
@@ -1,8 +0,0 @@
-@REM -------
-@REM make using Borland bcc32
-@REM type bmake, and wait...
-@REM do not edit - automatically generated from ProjectDefinition
-@REM -------
-make.exe -N -f bc.mak %*
-
-
--- ressources/cg_hdm_scheme2013-ClausGittinger.6.mcz_FILES/snapshot.stx/lccmake.bat
+++
@@ -1,8 +0,0 @@
-@REM -------
-@REM make using lcc compiler
-@REM type lccmake, and wait...
-@REM do not edit - automatically generated from ProjectDefinition
-@REM -------
-make.exe -N -f bc.mak -DUSELCC=1 %*
-
-
